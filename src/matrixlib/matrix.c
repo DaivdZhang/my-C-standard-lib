88f06aed8686edeb5dc76b73b11370c78d88b01a
DaivdZhang <DaivdZhang@users.noreply.github.com>
2016-10-23 10:43:51 +0800

    Update matrix.c

--- src/matrixlib/matrix.c
+++ src/matrixlib/matrix.c
@@ -3,6 +3,16 @@
 #include <math.h>
 #include <time.h>
 #include "matrix.h"
+#include "utility.h"
+
+
+int mdestroy(Matrix* mat)
+{
+    for(unsigned int i = 0; i < mat->shape[0]; i++)
+        free(mat->array[i]);
+    free(mat->array);
+    return 0;
+}
 
 
 Matrix zeros(unsigned int row, unsigned int col)
@@ -45,6 +55,26 @@ Matrix random(unsigned int row, unsigned int col)
         for(unsigned int j = 0; j < mat.shape[1]; j++)
             mat.array[i][j] = (double)rand()/RAND_MAX;
     return mat;
+}
+
+
+Matrix from_array(double* array, unsigned int row, unsigned int col)
+{
+    Matrix mat = zeros(row, col);
+
+    for(unsigned int i = 0; i < row*col; i++)
+        mat.array[i/col][i%col] = *(array + i);
+    return mat;
+}
+
+
+Matrix mcopy(Matrix new_mat, Matrix mat)
+{
+    for(unsigned int i = 0; i < new_mat.shape[0]; i++)
+        for(unsigned int j = 0; j < new_mat.shape[1]; j++)
+            new_mat.array[i][j] = mat.array[i][j];
+
+    return new_mat;
 }
 
 
@@ -132,6 +162,22 @@ Matrix nmul(Matrix mat, double k)
 }
 
 
+Matrix pmul(Matrix a, Matrix b)
+{
+    if(a.shape[0] != b.shape[0] || a.shape[1] != b.shape[1])
+    {
+        printf("cannot pmul two given matrixes.");
+        exit(303030);
+    }
+
+    Matrix c = zeros(a.shape[0], b.shape[1]);
+    for(unsigned int i = 0; i < a.shape[0]; i++)
+        for(unsigned int j = 0; j < a.shape[1]; j++)
+            c.array[i][j] = a.array[i][j]*b.array[i][j];
+    return c;
+}
+
+
 Matrix transpose(Matrix mat)
 {
     Matrix ret = zeros(mat.shape[1], mat.shape[0]);
@@ -140,4 +186,54 @@ Matrix transpose(Matrix mat)
         for(unsigned int i = 0; i < mat.shape[0]; i++)
             ret.array[j][i] = mat.array[i][j];
     return ret;
+}
+
+
+Matrix _transform(Matrix mat, int* flag)
+{    
+    Matrix _mat = zeros(mat.shape[0], mat.shape[1]);
+    mcopy(_mat, mat);
+    double k = 0;
+    short int count = 1;
+
+    for(unsigned int i = 0; i < _mat.shape[0]; i++)
+    {
+        if(_mat.array[i][i] == 0)
+        {
+            for(unsigned int _ = i + 1; _ < _mat.shape[0]; _++)
+                if(_mat.array[_][i] != 0)
+                {
+                    swap(_mat.array[_], _mat.array[i], _mat.shape[1]);
+                    count *= -1;
+                }
+        }
+        for(unsigned int _i = i + 1; _i < _mat.shape[0]; _i++)
+        {
+            k = _mat.array[_i][i]/_mat.array[i][i];
+            if(k == 0)
+                continue;
+            _mat.array[_i] = array_sub(_mat.array[_i], _mat.array[i], _mat.shape[1], k);
+        }
+    }
+    if(flag != NULL)
+        *flag = count;
+    return _mat;
+}
+
+
+double det(Matrix mat)
+{
+    int* flag = NULL;
+    if((flag = (int*)calloc(sizeof(int), 1)) == NULL)
+        exit(202020);
+    
+    Matrix _mat = zeros(mat.shape[0], mat.shape[1]);
+    double value = 1;
+    mcopy(_mat, _transform(mat, flag));
+
+    for(unsigned int i = 0; i < mat.shape[0]; i++)
+        value *= _mat.array[i][i];
+        
+    mdestroy(&_mat);
+    return value*(*flag);
 }
