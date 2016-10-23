3a971bd308d671bd59b5763e5a1e6490b405bc3d
akizunx <17682291+akizunx@users.noreply.github.com>
2016-10-23 10:42:41 +0800

    Update matrix.h

--- src/matrixlib/matrix.h
+++ src/matrixlib/matrix.h
@@ -8,16 +8,22 @@ struct _matrix{
 
 typedef struct _matrix Matrix;
 
+int mdestroy(Matrix*);
 Matrix zeros(unsigned int, unsigned int);
 Matrix eye(unsigned int);
 Matrix random(unsigned int, unsigned int);
+Matrix from_array(double*, unsigned int, unsigned int);
+
 void mprint(Matrix);
 
 Matrix madd(Matrix, Matrix);
 Matrix msub(Matrix, Matrix);
 Matrix mmul(Matrix, Matrix);
 Matrix nmul(Matrix, double);
+Matrix pmul(Matrix, Matrix);
 
 Matrix transpose(Matrix);
+Matrix _transform(Matrix, int*);
+double det(Matrix);
 
 #endif
