902a6fb43ed29a611e0317ea3d622257bdd9789f
akizunx <17682291+akizunx@users.noreply.github.com>
2016-10-23 10:40:13 +0800

    Add files via upload

---
+++ src/matrixlib/utility.h
@@ -0,0 +1,14 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+struct _ctuple{
+    void** elements;
+    unsigned int tsize;
+};
+
+typedef struct _ctuple CTuple;
+
+void swap(double*, double*, unsigned int);
+double* array_sub(double*, double*, unsigned int, double);
+
+#endif
